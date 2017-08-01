16042c8ed56a6755faf3d197a3ea784868dd209c
Roderick Fung <roderickfung@users.noreply.github.com>
2017-08-01 12:29:26 -0700

    life cycle events, directives, local databinding

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,3 +1,4 @@
+import { Recipe } from '../../../models/recipe.model';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -7,7 +8,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RecipeListComponent implements OnInit {
 
-  constructor() { }
+  // tslint:disable-next-line:max-line-length
+  onerecipe = new Recipe('A Test Recipe', 'Test Description 1', 'https://upload.wikimedia.org/wikipedia/commons/thumb/6/6b/Hapalos_Artos_%28soft_bread%29%2C_a_traditional_Ancient_Roman_recipe_for_a_classic_fine_bread%2C_from_Athenaeus%27_Deipnosophistae_%2816336232886%29.jpg/1024px-Hapalos_Artos_%28soft_bread%29%2C_a_traditional_Ancient_Roman_recipe_for_a_classic_fine_bread%2C_from_Athenaeus%27_Deipnosophistae_%2816336232886%29.jpg');
+
+  recipes: Recipe[] = [
+    this.onerecipe,
+    this.onerecipe
+  ];
+
+  constructor() {}
 
   ngOnInit() {
   }
