b07783deb322c12179e097bcaccbd325a610d675
Roderick Fung <roderickfung@users.noreply.github.com>
2017-08-02 15:20:38 -0700

    if statement navigation (temporary)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,4 +5,13 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {}
+export class AppComponent {
+
+  public page = 'recipes';
+
+  public onNavigate(link: string) {
+    console.log('what is link:', link);
+    this.page = link;
+  }
+
+}
--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,15 +1,18 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Output, EventEmitter} from '@angular/core';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css']
 })
-export class HeaderComponent implements OnInit {
+export class HeaderComponent {
 
-  constructor() { }
+  @Output() public pageName = new EventEmitter<string>();
 
-  ngOnInit() {
+  constructor() {}
+
+  public onSelect(link: string) {
+    console.log('what is page:', link);
+    this.pageName.emit(link);
   }
-
 }
